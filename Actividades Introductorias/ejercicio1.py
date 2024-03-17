bbd04ce0d576453cc6e32238e02b9f312d3c8c3a
Facundo Peri <facundoperi01@gmail.com>
2024-03-17 18:04:09 -0300

    FEAT: Ejercicio 1 de actividades introductorias.

---
+++ Actividades Introductorias/ejercicio1.py
@@ -0,0 +1,4 @@
+# Desarrolla un programa que solicite al usuario que ingrese su edad y luego calcule y
+# muestre cuántos años le faltan para alcanzar los 100 años.
+edad = int(input('Ingrese su edad actual: '))
+print(f'Para alcanzar los 100 años a usted le faltan {100-edad} años')
